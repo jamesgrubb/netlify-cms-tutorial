05af8e53479a866d0743a6658b42b1d7221af2d5
jamesgrubb <jamesgrubb@mac.com>
2019-07-02 13:50:42 +0100

    Updating config

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,27 +1,26 @@
 import React from 'react';
-import { graphql } from 'gatsby';
 
-const Template = ({data}) => {
-const { markdownRemark } = data
-const { frontmatter, html } = markdownRemark
-
-return (
+export default function Template({
+  data,
+}) {
+  const { markdownRemark } = data;
+  const { frontmatter, html } = markdownRemark;
+  return (
     <div className="blog-post-container">
-        <div className="blog-post">
-            <h1>{frontmatter.title}</h1>
-            <h2>{frontmatter.date}</h2>
-            <div 
-            className="blog-post-content"
-            dangerouslySetInnerHTML={{ __html: html}}
-            ></div>
-        </div>
+      <div className="blog-post">
+        <h1>{frontmatter.title}</h1>
+        <h2>{frontmatter.date}</h2>
+        <div
+          className="blog-post-content"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      </div>
     </div>
-)
-
+  );
 }
 
 export const pageQuery = graphql`
-query($path: String!) {
+  query BlogPostByPath($path: String!) {
     markdownRemark(frontmatter: { path: { eq: $path } }) {
       html
       frontmatter {
@@ -31,5 +30,4 @@ query($path: String!) {
       }
     }
   }
-`
-export default Template
+`;
